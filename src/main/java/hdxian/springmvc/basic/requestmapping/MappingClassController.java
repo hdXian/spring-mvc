375ac1a021a0a974749cfaa97fee8d991cf8de71
hdXian <hyun353500@naver.com>
2024-08-08 01:05:21 +0900

    #1 section 1-2 - add user management API example code

---
+++ src/main/java/hdxian/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,43 @@
+package hdxian.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    /**
+     * user management API example (only mapping example)
+     * get user list: GET /users
+     * add new user: POST /users
+     * get user by id: GET /users/{userId}
+     * modify user: PATCH /users/{userId}
+     * remove user: DELETE /users/{userId}
+     */
+
+    @GetMapping
+    public String users() {
+        return "get /users";
+    }
+
+    @PostMapping
+    public String addUser() {
+        return "post /users";
+    }
+
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable("userId") String userId) {
+        return "get userId=" + userId;
+    }
+
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable("userId") String userId) {
+        return "patch userId=" + userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable("userId") String userId) {
+        return "delete userId=" + userId;
+    }
+
+}
